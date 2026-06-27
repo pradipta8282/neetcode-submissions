3863ea0fd12e61d93eb403968e6a81753903c4d8
pradipta8282 <69620750+pradipta8282@users.noreply.github.com>
2026-06-28 01:01:13 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+          Dictionary<string, List<string>> map = new Dictionary<string, List<string>>();
+
+        foreach (string str in strs)
+        {
+            char[] chars = str.ToCharArray();
+            Array.Sort(chars);
+
+            string key = new string(chars);
+
+            if (!map.ContainsKey(key))
+            {
+                map[key] = new List<string>();
+            }
+
+            map[key].Add(str);
+        }
+
+        return map.Values.ToList();
+    }
+}
